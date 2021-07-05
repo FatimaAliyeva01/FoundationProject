c6799e35d83330126330e85c892a2d872d6bc37d
Fatima <fatimeeliyeva01@gmail.com>
2021-07-05 13:25:19 +0400

    Update

--- JavaScript/app2.js
+++ JavaScript/app2.js
@@ -1,11 +1,12 @@
-let div = document.createElement('div');
-document.querySelector('body').appendChild(div);
-div.classList.add('block');
-div.setAttribute('style','size:400px');
-for(i = 0; i< 10;i++){
-    let child = document.createElement('div');
-    document.querySelector('.block').appendChild(child);
-    child.classList.add('miniBlock');
-    child.innerText = i +1;
 
-} 
+
+        let div = document.createElement('div');
+div.classList.add('block');
+document.querySelector('body').appendChild(div);
+
+for (let i=0; i<10; i++){
+    let diviki = document.createElement('div');
+    document.querySelector('div').appendChild(diviki);
+    diviki.innerText = i+1;
+    diviki.classList.add('miniBlock');
+}
